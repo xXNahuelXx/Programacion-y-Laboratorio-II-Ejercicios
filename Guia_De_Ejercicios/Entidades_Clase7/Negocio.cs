9a6f1ce06e64788e4c8047042cdb308dc20f3f67
xXNahuelXx <nahuugh98@gmail.com>
2022-04-11 21:56:30 -0300

    Subiendo Ej1 Clase 7

---
+++ Guia_De_Ejercicios/Entidades_Clase7/Negocio.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Entidades_Clase7
+{
+    public class Negocio
+    {
+        private PuestoAtencion caja;
+        private Queue<Cliente> clientes;
+        private string nombre;
+
+        private Negocio()
+        {
+            this.clientes = new Queue<Cliente>();
+            this.caja = new PuestoAtencion(PuestoAtencion.Puesto.Caja1);
+        }
+
+        public Negocio(string nombre) : this()
+        {
+            this.nombre = nombre;
+        }
+
+        public Cliente Cliente
+        {
+            get
+            {
+                return this.clientes.Dequeue();
+            }
+
+            set
+            {
+                bool resultado = this + value;
+            }
+        }
+
+        public int ClientesPendientes
+        {
+            get
+            {
+                return this.clientes.Count();
+            }
+        }
+
+        public static bool operator +(Negocio n, Cliente c)
+        {
+            if (n != c)
+            {
+                n.clientes.Enqueue(c);
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator ==(Negocio n, Cliente c)
+        {
+            foreach (Cliente item in n.clientes)
+            {
+                if (item == c)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator !=(Negocio n, Cliente c)
+        {
+            return !(n == c);
+        }
+
+        public static bool operator ~(Negocio n)
+        {
+            if(n.ClientesPendientes > 0)
+            {
+                return n.caja.Atender(n.Cliente);
+            }
+            return false;
+        }
+    }
+}
---
+++ Guia_De_Ejercicios/Entidades_Clase7/PuestoAtencion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Entidades_Clase7
+{
+    public class PuestoAtencion
+    {
+        public enum Puesto
+        {
+            Caja1,
+            Caja2,
+        }
+
+        private static int numeroActual;
+        private Puesto puesto;
+
+        static PuestoAtencion()
+        {
+            PuestoAtencion.numeroActual = 0;
+        }
+
+        public PuestoAtencion(Puesto puesto)
+        {
+            this.puesto = puesto;
+        }
+        public static int NumeroActual
+        {
+            get 
+            {
+                return ++PuestoAtencion.numeroActual; 
+            }
+
+        }
+
+        public bool Atender(Cliente cli)
+        {
+            Thread.Sleep(5000);
+            return true;
+        }
+
+    }
+}
